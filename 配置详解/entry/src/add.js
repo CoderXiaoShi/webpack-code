0c4d8b210ef09ede265cbed285b2f6c549fae24b
石兴龙 <1003865572@qq.com>
2021-03-28 13:53:26 +0800

    update

--- 配置详解/entry/src/add.js
+++
@@ -1,3 +0,0 @@
-
-
-export default (x, y) => x + y
--- 配置详解/entry/src/index.js
+++
@@ -1,9 +0,0 @@
-import add from './add'
-import subtract from './subtract'
-
-let helloword = 'hello word'
-console.log(helloword)
-
-console.log(`add: ${add(1, 2)}`)
-console.log(`subtract: ${subtract(1, 2)}`)
-
